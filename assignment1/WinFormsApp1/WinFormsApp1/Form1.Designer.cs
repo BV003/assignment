a4f73eb53de491256d13fbc014434178cbcf95c9
BV003 <bbz975749@gmail.com>
2024-03-05 19:11:37 +0800

    1

---
+++ assignment1/WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            comboBox1 = new ComboBox();
+            label4 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(107, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 24);
+            label1.TabIndex = 4;
+            label1.Text = "输入1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(107, 223);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 24);
+            label2.TabIndex = 5;
+            label2.Text = "输入2";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(241, 41);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 30);
+            textBox1.TabIndex = 6;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(241, 223);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 30);
+            textBox2.TabIndex = 7;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(126, 577);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 24);
+            label3.TabIndex = 8;
+            label3.Text = "输出";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(227, 574);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(150, 30);
+            textBox3.TabIndex = 9;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "+", "-", "*", "/" });
+            comboBox1.Location = new Point(206, 410);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(182, 32);
+            comboBox1.TabIndex = 10;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(89, 405);
+            label4.Name = "label4";
+            label4.Size = new Size(82, 24);
+            label4.TabIndex = 11;
+            label4.Text = "选择符号";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(659, 427);
+            button1.Name = "button1";
+            button1.Size = new Size(195, 64);
+            button1.TabIndex = 12;
+            button1.Text = "计算";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1861, 819);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "简易运算器";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private ComboBox comboBox1;
+        private Label label4;
+        private Button button1;
+    }
+}
---
+++ assignment1/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,80 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            double num1 = Int32.Parse(textBox1.Text);
+            double num2 = Int32.Parse(textBox2.Text);
+            string o = comboBox1.Text;
+            double res = 0.00;
+            switch (o)
+            {
+
+                case "+":
+                    res = num1 + num2;
+                    break;
+                case "-":
+                    res = num1 - num2;
+                    break;
+                case "*":
+                    res = num1 * num2;
+                    break;
+                case "/":
+                    res = num1 / num2;
+                    break;
+            }
+            textBox3.Text = String.Format("{0:F2}", res);
+        }
+    }
+}
